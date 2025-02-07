13b7a3b3c9432072450cab4bcdd7e2df81134bce
= <=>
2025-02-07 14:23:30 -0800

    updated policy so terraform can create the OIDC provider if needed

--- src/1-providers.tf
+++ src/1-providers.tf
@@ -9,3 +9,26 @@ resource "aws_iam_openid_connect_provider" "terraform_oidc" {
     "6A55F24DD3DD27CA0279D8DC517467A61FCC1AB9"  # Terraform Cloud's SSL thumbprint
   ]
 }
+
+resource "aws_iam_policy" "allow_oidc_creation" {
+  name        = "AllowOIDCCreationPolicy"
+  description = "Allow creation of OIDC provider"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "iam:CreateOpenIDConnectProvider",
+      "Resource": "arn:aws:iam::736654693886:oidc-provider/app.terraform.io"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_oidc_creation_policy" {
+  role       = "oidc-example-role"
+  policy_arn = aws_iam_policy.allow_oidc_creation.arn
+}
